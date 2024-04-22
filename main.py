90de25d3acde804635f121146f6247933155ce43
Dovydas Lozys <755086+DovLozys@users.noreply.github.com>
2024-04-22 11:04:07 +0100

    prettifies the report that is printed

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 def main():
-    contents = get_file_content("books/frankenstein.txt")
+    path = "books/frankenstein.txt"
+
+    print(f"=== Begin report of {path} ===")
+
+    contents = get_file_content(path)
     if len(contents) < 1:
+        print("Document seems to be empty, end of report.")
         return
-    
-    print(count_words(contents))
-    print(count_letters(contents))
+
+    print(f"{count_words(contents)} words found in the document\n")
+
+    pretty_print_letter_counts(count_letters(contents))
+
+    print("\n=== End report ===")
 
 def get_file_content(path):
     try:
@@ -34,5 +42,18 @@ def count_letters(text):
     
     return counts
 
+def sort_on(dict):
+    return dict["count"]
+
+def pretty_print_letter_counts(letter_counts):
+    counts = []
+    for letter, count in letter_counts.items():
+        if letter.isalpha():
+            counts.append({"letter": letter, "count": count})
+
+    counts.sort(key=sort_on, reverse=True)
+    
+    for count in counts:
+        print(f"Letter {count["letter"]} was found {count["count"]:5} times.")
 
 main()
