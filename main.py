56d32d68f994190d489296b676c67a56dfaded24
Dovydas Lozys <755086+DovLozys@users.noreply.github.com>
2024-04-22 10:15:17 +0100

    adds error handling, word counting

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        contents = f.read()
-        print(len(contents.split()))
+    contents = get_file_content("books/frankenstein.txt")
+    if len(contents) < 1:
+        return
+    
+    print(count_words(contents))
+
+def get_file_content(path):
+    try:
+        with open(path) as f:
+            return f.read()
+    except Exception as e:
+        print(f"Error opening file: {path}\n")
+        print(e)
+        return ""
+
+def count_words(text):
+    if type(text) == str:
+        return len(text.split())
+    return 0
 
 main()
